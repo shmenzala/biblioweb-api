5d9782c8f16898d84283555bd3bd9c32df3b3d64
Samuel <shmenzala@gmail.com>
2023-08-18 18:55:59 -0500

    Initial commit

---
+++ Biblioapi/src/main/java/com/pe/sh/Biblioapi/BiblioapiApplication.java
@@ -0,0 +1,13 @@
+package com.pe.sh.Biblioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioapiApplication.class, args);
+	}
+
+}
---
+++ Biblioapi/src/test/java/com/pe/sh/Biblioapi/BiblioapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.sh.Biblioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
