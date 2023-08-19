25ed0fd687d2e879fb59e8d1423d23354cb426ae
Samuel <shmenzala@gmail.com>
2023-08-18 19:19:33 -0500

    Add entity classes (model package)

---
+++ Biblioapi/src/main/java/com/pe/sh/Biblioapi/model/Roles.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pe.sh.Biblioapi.model;
+
+/**
+ *
+ * @author shmen
+ */
+public class Roles {
+    
+}
