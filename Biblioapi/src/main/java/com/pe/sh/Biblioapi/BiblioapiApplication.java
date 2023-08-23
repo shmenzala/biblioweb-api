7f955771763c9b11a236a5c315ade01b806b1530
Samuel <shmenzala@gmail.com>
2023-08-22 19:02:10 -0500

    Add initial modelmapper configuration

--- Biblioapi/src/main/java/com/pe/sh/Biblioapi/BiblioapiApplication.java
+++ Biblioapi/src/main/java/com/pe/sh/Biblioapi/BiblioapiApplication.java
@@ -1,11 +1,18 @@
 package com.pe.sh.Biblioapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BiblioapiApplication {
 
+        @Bean
+        public ModelMapper modelMapper(){
+            return new ModelMapper();
+        }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(BiblioapiApplication.class, args);
 	}
