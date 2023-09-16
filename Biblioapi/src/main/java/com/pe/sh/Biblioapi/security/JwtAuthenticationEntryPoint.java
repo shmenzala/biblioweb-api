6d2b17bd6d5c71ffd02f5993072d68a043fe1cad
Samuel <shmenzala@gmail.com>
2023-09-16 11:47:09 -0500

    Add classes for JWT security operation (jwtHandler)

--- Biblioapi/src/main/java/com/pe/sh/Biblioapi/security/JwtAuthenticationEntryPoint.java
+++ Biblioapi/src/main/java/com/pe/sh/Biblioapi/security/JwtAuthenticationEntryPoint.java
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
-    
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
