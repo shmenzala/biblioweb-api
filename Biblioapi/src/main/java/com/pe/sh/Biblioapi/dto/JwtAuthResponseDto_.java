51f18aac65b191626ba0745ea8b68a05c4ca70a4
Samuel <shmenzala@gmail.com>
2023-09-16 11:55:14 -0500

    Add DTO classes for user authentication

---
+++ Biblioapi/src/main/java/com/pe/sh/Biblioapi/dto/JwtAuthResponseDto_.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pe.sh.Biblioapi.dto;
+
+/**
+ *
+ * @author shmen
+ */
+public class JwtAuthResponseDto_ {
+
+    private String tokenDeAcceso;
+    private String tipoDeToken = "Bearer";
+
+    public JwtAuthResponseDto_() {
+    }
+    
+    public JwtAuthResponseDto_(String tokenDeAcceso) {
+        this.tokenDeAcceso = tokenDeAcceso;
+    }
+
+    public String getTokenDeAcceso() {
+        return tokenDeAcceso;
+    }
+
+    public void setTokenDeAcceso(String tokenDeAcceso) {
+        this.tokenDeAcceso = tokenDeAcceso;
+    }
+
+    public String getTipoDeToken() {
+        return tipoDeToken;
+    }
+
+    public void setTipoDeToken(String tipoDeToken) {
+        this.tipoDeToken = tipoDeToken;
+    }
+
+}
