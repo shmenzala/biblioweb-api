1246f1f909135d49ada60dd821c80816a75354b6
Samuel <shmenzala@gmail.com>
2023-10-27 15:00:08 -0500

    Add new exception handling section for "MethodArgumentNotValidException"

--- Biblioapi/src/main/java/com/pe/sh/Biblioapi/exceptions/GlobalExceptionHandler.java
+++ Biblioapi/src/main/java/com/pe/sh/Biblioapi/exceptions/GlobalExceptionHandler.java
@@ -5,8 +5,11 @@
 package com.pe.sh.Biblioapi.exceptions;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -17,23 +20,34 @@ import org.springframework.web.context.request.WebRequest;
  */
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    
+
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorDetails> resourceNotFoundException(ResourceNotFoundException ex, WebRequest webRequest){
+    public ResponseEntity<ErrorDetails> resourceNotFoundException(ResourceNotFoundException ex, WebRequest webRequest) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
-    
+
     @ExceptionHandler(BiblioapiAppException.class)
-    public ResponseEntity<ErrorDetails> vetAppException(BiblioapiAppException ex, WebRequest webRequest){
+    public ResponseEntity<ErrorDetails> vetAppException(BiblioapiAppException ex, WebRequest webRequest) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
     
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDetailsValidConstraints> validContraints(MethodArgumentNotValidException ex, WebRequest webRequest) {
+        Map<String, String> errorMap = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errorMap.put(error.getField(), error.getDefaultMessage());
+        });
+        
+        ErrorDetailsValidConstraints errorVC = new ErrorDetailsValidConstraints(new Date(), errorMap, webRequest.getDescription(false));
+        return new ResponseEntity<>(errorVC, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorDetails> globalException(Exception ex, WebRequest webRequest){
+    public ResponseEntity<ErrorDetails> globalException(Exception ex, WebRequest webRequest) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-    
+
 }
